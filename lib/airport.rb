02fbec4eeeb7fd6c7ae39f3bf303362fd1dcd308
Max <max.francis2000@gmail.com>
2018-10-05 11:33:35 +0000

    Starting the challenge

---
+++ lib/airport.rb
@@ -0,0 +1,7 @@
+class Airplane
+  
+  def hello
+    "hello"
+  end
+  
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,8 @@
+require 'airport'
+
+describe Airplane do
+  
+  it 'respond hello' do
+    expect(subject.hello).to eq "hello"
+  end
+end
